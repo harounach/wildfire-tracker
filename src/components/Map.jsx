88b0bea9517636744715fd285fff7bf727de496a
Haroun Ach <harounacho21@gmail.com>
2021-10-12 21:11:45 +0100

    Plot fire markers on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import FireIcon from "./FireIcon";
 
+const NATURAL_EVENT_WILDFIRE = 8;
+
 const Map = ({ data, center, zoom }) => {
+  const markers = data.map((ev, index) => {
+    const lat = ev.geometries[0].coordinates[1];
+    const lng = ev.geometries[0].coordinates[0];
+
+    if (ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
+      return (
+        <Marker key={index} position={[lat, lng]} icon={FireIcon}>
+          <Popup>{ev.title}</Popup>
+        </Marker>
+      );
+    }
+    return null;
+  });
+
   return (
     <div className="w-full flex-grow">
       <MapContainer
@@ -15,11 +31,7 @@ const Map = ({ data, center, zoom }) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[center.lat, center.lng]} icon={FireIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {markers}
       </MapContainer>
     </div>
   );
